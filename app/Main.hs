3d428402d5f4e161ae4730d204ade38537003058
ardeleanasm <ardeleanasm@gmail.com>
2024-08-15 17:33:27 +0300

    updated

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,15 @@ import QC
 
 main :: IO ()
 main = do
-  let machine = Machine (makeQuantumState 2) 0
+  let machine = Machine (makeQuantumState 10) 0
   let qprog = QProgram [
         (QInstruction hGate [0])
         , (QInstruction hGate [1])
+        , (QInstruction hGate [2])
+        , (QInstruction hGate [3])
+        , (QInstruction hGate [4])
+        , (QInstruction hGate [5])
+                                        
         ]
   machines <- replicateM 10 (runQProg qprog machine)
   --mapM_ print machines
